d368bcd62ddbbe3c94dfc03b5af61e9815c3442b
agaos <agaoskar0827@gmail.com>
2023-08-15 01:51:34 +0200

    Add project files.

---
+++ Zipperoni/Main.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Zipperoni
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            EnterButton = new Button();
+            TextBox = new Label();
+            Num1Box = new TextBox();
+            Num2Box = new TextBox();
+            progressBar = new ProgressBar();
+            label1 = new Label();
+            label2 = new Label();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            contextMenuStrip2 = new ContextMenuStrip(components);
+            toolStripTextBox1 = new ToolStripTextBox();
+            contextMenuStrip2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // EnterButton
+            // 
+            EnterButton.Location = new Point(70, 109);
+            EnterButton.Name = "EnterButton";
+            EnterButton.Size = new Size(147, 36);
+            EnterButton.TabIndex = 0;
+            EnterButton.Text = "Calculate!";
+            EnterButton.UseVisualStyleBackColor = true;
+            EnterButton.Click += button1_Click;
+            // 
+            // TextBox
+            // 
+            TextBox.AutoSize = true;
+            TextBox.Location = new Point(125, 10);
+            TextBox.Name = "TextBox";
+            TextBox.Size = new Size(35, 15);
+            TextBox.TabIndex = 1;
+            TextBox.Text = "Hello";
+            TextBox.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Num1Box
+            // 
+            Num1Box.Location = new Point(17, 51);
+            Num1Box.Name = "Num1Box";
+            Num1Box.Size = new Size(100, 23);
+            Num1Box.TabIndex = 2;
+            // 
+            // Num2Box
+            // 
+            Num2Box.Location = new Point(170, 51);
+            Num2Box.Name = "Num2Box";
+            Num2Box.Size = new Size(100, 23);
+            Num2Box.TabIndex = 3;
+            // 
+            // progressBar
+            // 
+            progressBar.ForeColor = SystemColors.HighlightText;
+            progressBar.Location = new Point(17, 80);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(253, 23);
+            progressBar.TabIndex = 4;
+            progressBar.Click += progressBar_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(34, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Number 1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(194, 33);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Number 2";
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            contextMenuStrip2.Items.AddRange(new ToolStripItem[] { toolStripTextBox1 });
+            contextMenuStrip2.Name = "contextMenuStrip2";
+            contextMenuStrip2.Size = new Size(161, 29);
+            // 
+            // toolStripTextBox1
+            // 
+            toolStripTextBox1.Name = "toolStripTextBox1";
+            toolStripTextBox1.Size = new Size(100, 23);
+            toolStripTextBox1.Click += toolStripTextBox1_Click;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(295, 182);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(progressBar);
+            Controls.Add(Num2Box);
+            Controls.Add(Num1Box);
+            Controls.Add(TextBox);
+            Controls.Add(EnterButton);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Main";
+            Text = "Quantum Calculator";
+            Load += Main_Load;
+            contextMenuStrip2.ResumeLayout(false);
+            contextMenuStrip2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button EnterButton;
+        private Label TextBox;
+        private TextBox Num1Box;
+        private TextBox Num2Box;
+        private ProgressBar progressBar;
+        private Label label1;
+        private Label label2;
+        private ContextMenuStrip contextMenuStrip1;
+        private ContextMenuStrip contextMenuStrip2;
+        private ToolStripTextBox toolStripTextBox1;
+    }
+}
---
+++ Zipperoni/Main.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Zipperoni
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer soundBull = new SoundPlayer(Properties.Resources.bullets);
+            
+            SetStartup();
+
+            Random rnd = new Random();
+            progressBar.Value = 0;
+
+            int num1;
+            int num2;
+            try
+            {
+                num1 = int.Parse(Num1Box.Text);
+            }
+            catch
+            {
+                System.Media.SystemSounds.Exclamation.Play();
+                soundBull.Play();
+                MessageBox.Show("Invalid number in field 1", "Error!");
+                return;
+            }
+
+            try
+            {
+                num2 = int.Parse(Num2Box.Text);
+            }
+            catch
+            {
+                System.Media.SystemSounds.Exclamation.Play();
+                soundBull.Play();
+                MessageBox.Show("Invalid number in field 2", "Error!");
+                return;
+            }
+
+
+            int result = num1 + num2;
+
+            TextBox.Text = "Calcuating...";
+
+            EnterButton.Enabled = false;
+            Num1Box.Enabled = false;
+            Num2Box.Enabled = false;
+
+            SoundPlayer soundNick = new SoundPlayer(Properties.Resources.intro);
+            SoundPlayer soundWin = new SoundPlayer(Properties.Resources.Usher);
+            soundWin.Stop();
+            soundNick.PlayLooping();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int time = rnd.Next(250, 1500);
+                await Task.Delay(time);
+                progressBar.Value = i;
+                System.Media.SystemSounds.Beep.Play();
+            }
+            soundNick.Stop();
+            soundWin.PlayLooping();
+
+
+            progressBar.Value = 100;
+
+            EnterButton.Enabled = true;
+            Num1Box.Enabled = true;
+            Num2Box.Enabled = true;
+
+            TextBox.Text = result.ToString();
+            System.Media.SystemSounds.Exclamation.Play();
+            MessageBox.Show("Your answer is: " + result + "!");
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void SetStartup()
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+
+            rk.SetValue("Zipperoni", Application.ExecutablePath);
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
